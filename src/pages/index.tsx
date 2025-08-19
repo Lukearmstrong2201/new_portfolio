70881968b305571550753fb48f8587bb6b34ca54
Luke <lukearmstrong2201@hotmail.com>
2025-08-18 23:02:54 -0400

    add hero component to index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
-import Navigation from '../components/navigation'
+import Navigation from '../components/Navigation'
+import Hero from '../components/Hero';
 import Footer from '../components/Footer'
 
 const Index = () => {
     return (
       <div className="min-h-screen bg-background">
         <Navigation />
+        <main>
+          <section id="home">
+            <Hero />
+          </section>
+        </main>
         <Footer />
       </div>
     );
